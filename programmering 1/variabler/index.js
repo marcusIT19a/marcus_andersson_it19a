acf41f7c9aadce3b75c4e9702d57e1659584cbf8
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2020-09-10 08:37:44 +0200

    bya variabler

---
+++ programmering 1/variabler/index.js
@@ -0,0 +1,23 @@
+// skapa en variabel name och tilldela det ditt namn
+let name = "marcus"
+
+
+//skriv ut "Hej namn"
+console.log(`Hej ${name}`)
+
+// 1. skapa variablerna tal1, tal2, tal3
+// 2. tilldela dessa variabler med värderna 3,4,5
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+
+// 3. räkna ut medelvärdet av dem 
+medelvarde = (tal1+tal2+tal3)/3
+
+// 4. Gör följande utskrift: 
+//"Medelvärdet av talen 3,4,5 är 4" 
+console.log(`(${tal1}+${tal2}+${tal3})/3 = ${medelvarde}`)
+
+let langd = prompt("ange värdet för rektangelns längd ")
+
+

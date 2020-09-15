7ab0933e8572ce1204ab4209118341109b09ada5
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2020-09-15 09:40:31 +0200

    hej

---
+++ programmering 1/If-satser/if_sats1.js
@@ -0,0 +1,31 @@
+// modulusoperator
+console.log(`${5%2}`)
+
+let tal = 20
+
+// avgöra ifall talet är jämnt eller inte
+
+// tal1%2 är 0 = jämnt
+// tal1%2 är 1 = udda
+
+if (tal%2 === 0){
+    console.log("jämnt")
+} else {
+    console.log ("udda")
+}
+
+// Uppgift
+// 1. låt användaren skriva in ett tal
+// 2. avgör om talet är positivt eller negativt
+
+let tal1  = prompt ("ange ett tal: ")
+
+if (tal1 > 0 )
+    console.log("positivt")
+    {
+        else if (tal1 == 0)
+        console.log 
+    }
+} else {
+    console.log("Negativt")
+}

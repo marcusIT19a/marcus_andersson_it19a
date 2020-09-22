8484a07d669d460768115f2dba8702725dd06012
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2020-09-22 09:25:44 +0200

    Handbagage IFs

---
+++ programmering 1/If-satser/if_sats3.js
@@ -0,0 +1,36 @@
+let vikt = prompt("Hur mycket väger din väska?")
+
+if (vikt >= 8){
+console.log ("Vikten på din väska är inte godkänd")
+} else {
+    console.log ("Vikten på din väska är godkänd")
+}
+
+let längd = prompt("Hur lång är din väska?")
+
+if (längd >= 55){
+console.log ("Längden på din väska är inte godkänd")
+} else {
+    console.log ("Längden på din väska är godkänd")
+}
+
+let bredd = prompt("hur bred är din väska?")
+
+if (bredd >= 40){
+    console.log ("Bredden på din väska är inte godkänd")
+    } else {
+        console.log ("Bredden på din väska är godkänd")
+    }
+
+    let höjd = prompt("hur hög är din väska?")
+
+    if (höjd >= 23){
+        console.log ("Höjden på din väska är inte godkänd")
+        } else {
+            console.log ("Höjden på din väska är godkänd")
+        }
+
+
+
+
+

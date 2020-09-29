8f1840ec7fb05893003362906640fc155577e3c2
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2020-09-29 08:43:01 +0200

    for sats

---
+++ programmering 1/For-sats/for1.js
@@ -0,0 +1,22 @@
+let p_talserie1 = document.querySelector("#talserie1")
+let p_talserie2 = document.querySelector("#talserie2")
+let p_summa = document.querySelector("#summa")
+let summa = 0
+
+for (let i=1; i<=10; i++){
+    p_talserie1.innerHTML += `${i} `
+}
+
+for (let j=10; j>=0; j--){
+    p_talserie2.innerHTML += `${j} `
+}
+
+for (let i = 0; i<10; i++){
+    console.log(`${i}. I will not write all over the walls`)
+}
+
+// vill räkna ut summan 1+2+3+...+8+9+10
+for (let i=0; i<=10; i++){
+    summa += i
+}
+p_summa.innerHTML += `1 + 2 +3 +...+9+10 = ${summa}`

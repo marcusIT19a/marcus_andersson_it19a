2b2b3a7d3830ea26d45aba66fa1aadfa2b731dce
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2020-11-12 09:43:16 +0100

    strings

---
+++ programmering 1/Strings/strings.js
@@ -0,0 +1,24 @@
+// konkatenering
+
+let förnamn = "Marcus"
+let efternamn = "Andersson"
+let namn = förnamn + " " +  efternamn 
+let adress = "Badviksvägen 42"
+let telefon = "0707648538"
+let alder = "17"
+
+console.log("Namn: " + namn + "\n" + "Ålder: " + alder + "\n" + "Adress: " + adress + "\n" + "Telefon: " + telefon + "\n")
+
+let p_uppgifter = document.querySelector("#Personuppgifter")
+p_uppgifter.innerHTML = "Namn: " + namn + "<br>" + "Ålder: " + alder + "<br>" + "Adress: " + adress + "<br>" + "Telefon: " + telefon 
+
+let alfabet = "abcdefghijklmnoprstuvwxyzåäö"
+let bokstav = alfabet[0]
+let bokstav = alfabet[4]
+
+console.log(`Bokstav på index 0 är: ${bokstav1}`)
+console.log(`Bokstav på index 4 är: ${bokstav5}`)
+
+
+let antal_bokstaver = alfabet.length
+console.log( `Antal bokstäver i alfabetet är ${antal_bokstaver}`)

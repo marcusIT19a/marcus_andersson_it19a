76c7262c6b651f3a20f64466d3fbac92e127d250
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2020-11-24 09:35:13 +0100

    hej

--- programmering 1/Inlämningsuppgifter/for2.js
+++ programmering 1/Inlämningsuppgifter/for2.js
@@ -1,4 +1,6 @@
 let p_ämnen = document.querySelector("#ämnen")
+let p_udda = document.querySelector("#udda")
+let p_jämna = document.querySelector("#jämna")
 
 let ämnen = ["Svenska", "Engelska", "Programmering", "Matematik", "Idrott"]
 
@@ -6,3 +8,7 @@ p_ämnen.innerHTML = "Ämnen <br/>"
 
 for (let k = 0; k < ämnen.length; k++){
 p_ämnen.innerHTML += `${k+1}. ${ämnen [k]} <br/>`}
+
+for (let a=0)
+
+

8e3b52e6a0327b48976b71ca7bf0135aa84d3d7d
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2020-12-10 10:12:28 +0100

    dice dame

--- programmering 1/dice game/dice.js
+++ programmering 1/dice game/dice.js
@@ -6,12 +6,14 @@ let diceNumber1 = 0
 let diceNumber2 = 0
 let dice1_btn = document.querySelector("#dice_p1")
 let dice2_btn = document.querySelector("#dice_p2")
+let buttonCheck = document.querySelector("#buttonCheck")
 let p1_result = document.querySelector("#p1_result")
 let p2_result = document.querySelector("#p2_result")
 
 
 dice1_btn.addEventListener("click", diceRoll1)
 dice2_btn.addEventListener("click", diceRoll2)
+buttonCheck.addEventListener("click", checkWin)
 
 function diceRoll1(){
     diceNumber1 = 1+Math.floor(Math.random()*6)
@@ -23,3 +25,14 @@ function diceRoll2(){
     diceNumber2 = Math.floor(Math.random()*6)+1
     p2_result.innerHTML = diceNumber2
 } 
+
+function checkWin(){
+    if (diceNumber1 > diceNumber2){
+    console.log("Spelare 1 vinner")
+} else if (diceNumber1 < diceNumber2){
+    console.log("Spelare 2 vinner")
+} else {
+    console.log("Lika")
+}
+}
+

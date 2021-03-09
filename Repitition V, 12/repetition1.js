44aa4c052a1704b4d763cbfaf7d60ea829e9d3a9
marcusIT19a <70194239+marcusIT19a@users.noreply.github.com>
2021-03-09 09:06:30 +0100

    repetition V.10

---
+++ Repitition V, 12/repetition1.js
@@ -0,0 +1,24 @@
+console.log("repetetion V. 10")
+
+// skriva variabler
+
+let name = "Marcus"
+let age = 17
+
+console.log(`Hej ${name}, du är ${age} år gammal`)
+
+// if sats
+
+if (age >= 18){
+    console.log("Du är vuxen")
+}else {
+    console.log("Du är ett barn");
+}
+
+// for sats - skriva ut jämna tal mellan 0 och 100
+
+for (let i = 0; i < 20; i++){
+    if (i % 2==0){
+        console.log(i);
+    }
+}
